eb51f29bebeddfb67cd319eb7933f67797615fbd
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-07 20:35:41 +0800

    JavaWeb

--- src/main/java/com/web/controller/CalcAreaServlet.java
+++ src/main/java/com/web/controller/CalcAreaServlet.java
@@ -1,6 +1,7 @@
 package com.web.controller;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,10 +18,20 @@ public class CalcAreaServlet extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 接收參數
         String string_r = req.getParameter("r");
+        
+        // 2. 商業邏輯運算
         int r = Integer.parseInt(string_r);
         double area = Math.pow(r, 2) * Math.PI;
-        resp.getWriter().print(String.format("r: %d area: %.2f", r, area));
+        
+        // 3. 建立分派器與 jsp 位置
+        RequestDispatcher rd = req.getRequestDispatcher("/jsps/calcAreaResult.jsp");
+        // 3.1 新增/設定 request 屬性, 傳遞給 jsp 頁面使用
+        req.setAttribute("result", area);
+        // 3.2 傳送
+        rd.forward(req, resp);
+        //resp.getWriter().print(String.format("r: %d area: %.2f", r, area));
     }
     
 }
