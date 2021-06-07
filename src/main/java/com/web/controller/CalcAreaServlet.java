770f4b55c74800ff6024079f97321707c5dca4ad
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-07 20:00:04 +0800

    JavaWeb

--- src/main/java/com/web/controller/CalcAreaServlet.java
+++ src/main/java/com/web/controller/CalcAreaServlet.java
@@ -10,9 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/controller/CalcArea") // url-pattern
 public class CalcAreaServlet extends HttpServlet {
 
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.getWriter().print("Sorry~~");
+//    }
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("doPost OK");
+        String string_r = req.getParameter("r");
+        int r = Integer.parseInt(string_r);
+        double area = Math.pow(r, 2) * Math.PI;
+        resp.getWriter().print(String.format("r: %d area: %.2f", r, area));
     }
     
 }
