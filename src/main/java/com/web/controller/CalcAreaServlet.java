e6b4a1f544f35cde000ca48c24e9124cb5bfb112
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-07 20:54:55 +0800

    JavaWeb

--- src/main/java/com/web/controller/CalcAreaServlet.java
+++ src/main/java/com/web/controller/CalcAreaServlet.java
@@ -1,5 +1,6 @@
 package com.web.controller;
 
+import com.web.service.CalaAreaService;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,20 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/controller/CalcArea") // url-pattern
 public class CalcAreaServlet extends HttpServlet {
-
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        resp.getWriter().print("Sorry~~");
-//    }
+    
+    // 計算 service
+    private CalaAreaService service = new CalaAreaService();
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 1. 接收參數
+        String string_type = req.getParameter("type");
         String string_r = req.getParameter("r");
+        int type = Integer.parseInt(string_type);
+        int r = Integer.parseInt(string_r);
         
         // 2. 商業邏輯運算
-        int r = Integer.parseInt(string_r);
-        double area = Math.pow(r, 2) * Math.PI;
+        double area = service.getAreaResult(type, r);
         
         // 3. 建立分派器與 jsp 位置
         RequestDispatcher rd = req.getRequestDispatcher("/jsps/calcAreaResult.jsp");
