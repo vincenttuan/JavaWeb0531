0eea3c6cbb3909d68ca9047059ccb1c14734584f
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-11 19:20:03 +0800

    JavaWeb

--- src/main/java/com/web/controller/CalcAreaServlet.java
+++ src/main/java/com/web/controller/CalcAreaServlet.java
@@ -1,38 +1,49 @@
 package com.web.controller;
 
-import com.web.service.CalaAreaService;
+import com.web.service.CalcAreaService;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static jdk.nashorn.internal.objects.NativeJava.typeName;
 
 @WebServlet("/controller/CalcArea") // url-pattern
 public class CalcAreaServlet extends HttpServlet {
     
     // 計算 service
-    private CalaAreaService service = new CalaAreaService();
+    private CalcAreaService service = new CalcAreaService();
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 1. 接收參數
+        // 1. 接收單筆參數(單一名稱)
+        /*
         String string_type = req.getParameter("type");
         String string_r = req.getParameter("r");
         int type = Integer.parseInt(string_type);
         int r = Integer.parseInt(string_r);
+        */
+        
+        // 1. 接收多組參數(多重複名稱)
+        String[] types = req.getParameterValues("type");
+        String[] rs    = req.getParameterValues("r");
         
         // 2. 商業邏輯運算
-        double area = service.getAreaResult(type, r);
-        String typeName = service.getNameByType(type);
+        //double area = service.getAreaResult(type, r);
+        //String typeName = service.getNameByType(type);
+        List<Map> list = service.getAreaResults(types, rs);
         
         // 3. 建立分派器與 jsp 位置
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/calcAreaResult.jsp");
         // 3.1 新增/設定 request 屬性, 傳遞給 jsp 頁面使用
-        req.setAttribute("r", r);
-        req.setAttribute("result", String.format("%.2f", area));
-        req.setAttribute("typeName", typeName);
+        //req.setAttribute("r", r);
+        //req.setAttribute("result", String.format("%.2f", area));
+        //req.setAttribute("typeName", typeName);
+        req.setAttribute("list", list);
         // 3.2 傳送
         rd.forward(req, resp);
         //resp.getWriter().print(String.format("r: %d area: %.2f", r, area));
---
+++ src/main/java/com/web/service/CalcAreaService.java
@@ -0,0 +1,58 @@
+package com.web.service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CalcAreaService {
+    
+    private static Map<Integer, String> types;
+    static {
+        types = new LinkedHashMap<>();
+        types.put(1, "圓面積");
+        types.put(2, "球體積");
+    }
+    
+    public String getNameByType(int type) {
+        return types.get(type);
+    }
+    
+    public double getAreaResult(int type, int r) {
+        double area = 0;
+        
+        switch(type) {
+            case 1: // 圓面積
+                area = Math.pow(r, 2) * Math.PI;
+                break;
+            case 2: // 球體積
+                area = Math.pow(r, 3) * Math.PI * 4/3;
+                break;
+            default:
+                area = -1;
+        }
+        
+        return area;
+    }
+
+    
+    public List<Map> getAreaResults(String[] types, String[] rs) {
+        // [{r=10, area=314, name="圓面積"}, ...]
+        List<Map> list = new ArrayList<>();
+        for(int i=0;i<types.length;i++) {
+            if (rs[i] == null || rs[i].equals("")) {
+                types[i] = "0";
+            }
+            int type    = Integer.parseInt(types[i]);
+            int r       = Integer.parseInt(rs[i]);
+            String name = getNameByType(type);
+            double area = getAreaResult(type, r);
+            Map map = new LinkedHashMap();
+            map.put("r", r);
+            map.put("area", area);
+            map.put("name", name);
+            list.add(map);
+        }
+        return list;
+    }
+}
