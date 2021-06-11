38365f9144372f235cc2db10becb5eb3147e1b31
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-11 20:39:08 +0800

    JavaWeb

--- src/main/java/com/web/controller/CalcAreaServlet.java
+++ src/main/java/com/web/controller/CalcAreaServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static jdk.nashorn.internal.objects.NativeJava.typeName;
 
 @WebServlet("/controller/CalcArea") // url-pattern
 public class CalcAreaServlet extends HttpServlet {
---
+++ src/main/java/com/web/controller/RegisterServlet.java
@@ -0,0 +1,31 @@
+package com.web.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/controller/Register")
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // sno=A01&eno=1z0-900&time=1&time=3&pay=true&memo=GO
+        String sno    = req.getParameter("sno");
+        String eno    = req.getParameter("eno");
+        List<Integer> time = Stream.of(req.getParameterValues("time"))
+                                    .map(t -> Integer.parseInt(t))
+                                    .collect(toList());
+        Boolean pay   = Boolean.parseBoolean(req.getParameter("pay"));
+        String memo   = req.getParameter("memo");
+        
+        
+    }
+    
+}
