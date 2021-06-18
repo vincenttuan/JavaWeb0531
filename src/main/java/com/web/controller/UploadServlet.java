9273c163c82b91b241ed05032032e0d4bcc3f4ec
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-18 19:25:23 +0800

    JavaWeb

--- src/main/java/com/web/controller/UploadServlet.java
+++ src/main/java/com/web/controller/UploadServlet.java
@@ -2,6 +2,7 @@ package com.web.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 //@WebServlet(urlPatterns = {"/controller/upload/file", "/controller/upload/image"})
 @WebServlet("/controller/upload/*") // 後置路徑對應 * 代表任意字串(path info)
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 30
+)
 public class UploadServlet extends HttpServlet{
 
     @Override
