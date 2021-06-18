b63ee518be2731d7e65e8b9e13eca4c08e5e689d
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-18 19:38:40 +0800

    JavaWeb

--- src/main/java/com/web/controller/UploadServlet.java
+++ src/main/java/com/web/controller/UploadServlet.java
@@ -1,12 +1,14 @@
 package com.web.controller;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 
 //@WebServlet(urlPatterns = {"/controller/upload/file", "/controller/upload/image"})
 @WebServlet("/controller/upload/*") // 後置路徑對應 * 代表任意字串(path info)
@@ -42,7 +44,8 @@ public class UploadServlet extends HttpServlet {
                 .filter(part -> part.getName().equals("cname"))
                 .forEach(part -> {
                     try {
-                        String cname = part.getInputStream().toString();
+                        String cname = IOUtils.toString(part.getInputStream(), 
+                                                        StandardCharsets.UTF_8.name());
                         resp.getWriter().print(part.getName() + "<br />");
                         resp.getWriter().print(cname + "<br />");
                     } catch (Exception e) {
