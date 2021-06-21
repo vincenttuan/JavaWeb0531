41aaa0eddd40d34062bebc10f3503b34f396f756
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-21 21:34:19 +0800

    JavaWeb

---
+++ src/main/java/com/web/filter/PerformanceFiler.java
@@ -0,0 +1,20 @@
+package com.web.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PerformanceFiler extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        long begin = System.currentTimeMillis();
+        chain.doFilter(req, res);
+        long end = System.currentTimeMillis();
+        System.out.println(req.getServletPath() + " : " + (end-begin) + "ms");
+    }
+    
+}
