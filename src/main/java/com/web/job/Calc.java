5383cf6aa4ae300afcec67e1659eaedb2d9e765b
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-25 21:10:20 +0800

    JavaWeb

---
+++ src/main/java/com/web/job/Calc.java
@@ -0,0 +1,26 @@
+package com.web.job;
+
+public class Calc {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public int result() {
+        return x + y;
+    }
+}
