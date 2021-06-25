b5be71fe8860287671c51533fe504ce606add7df
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-25 19:45:00 +0800

    JavaWeb

---
+++ src/main/java/com/web/job/Lotto.java
@@ -0,0 +1,15 @@
+package com.web.job;
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class Lotto {
+    public Set<Integer> get539(int n) {
+        Set<Integer> nums = new LinkedHashSet<>();
+        while (nums.size() < 5) {            
+            nums.add(new Random().nextInt(39) + 1);
+        }
+        return nums;
+    }
+}
