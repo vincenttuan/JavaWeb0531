385ffec6eac18490d721249ef7ecf13233108789
vincenttuan <vincentjava@yahoo.com.tw>
2021-06-28 21:11:27 +0800

    JavaWeb

---
+++ src/main/java/com/web/vo/Person.java
@@ -0,0 +1,42 @@
+package com.web.vo;
+
+public class Person {
+    
+    private String name;
+    private String sex;
+    private int score;
+
+    public Person() {
+    }
+
+    public Person(String name, String sex, int score) {
+        this.name = name;
+        this.sex = sex;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+}
