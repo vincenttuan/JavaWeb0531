e0c8d6d06f548470d147d48b67ecc49bd24d6b9c
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-09 19:39:21 +0800

    JavaWeb

--- src/main/java/com/web/tag/BMITag.java
+++ src/main/java/com/web/tag/BMITag.java
@@ -1,5 +1,10 @@
 package com.web.tag;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+
 /*
 Tag:
   1. <my:bmi h="170" w="60" />
@@ -8,6 +13,69 @@ OUTPUT:
   1. h=170 w=60 bmi=20.76
   2. h=170 w=60 bmi=20.76 result="正常" (ex:太瘦，太胖...)
 */
-public class BMITag {
+public class BMITag implements Tag {
+    private PageContext pageContext;
+    private Tag parentTag;
+    private double h, w;
+    private boolean result;
+    @Override
+    public void setPageContext(PageContext pc) {
+        pageContext = pc;
+    }
+
+    @Override
+    public void setParent(Tag t) {
+        parentTag = t;
+    }
+
+    @Override
+    public Tag getParent() {
+        return parentTag;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+
+    public void setW(double w) {
+        this.w = w;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    
+    @Override
+    public int doStartTag() throws JspException {
+        return Tag.SKIP_BODY;
+    }
+
+    @Override
+    public int doEndTag() throws JspException {
+        double bmi = w / Math.pow(h/100, 2);
+        try {
+            JspWriter out = pageContext.getOut();
+            out.print(String.format("h=%.1f w=%.1f bmi=%.2f ", h, w, bmi));
+            if(result) {
+                out.print("result=");
+                if(bmi >= 18) {
+                    out.print("太瘦");
+                } else if(bmi > 23) {
+                    out.print("太胖");
+                } else {
+                    out.print("正常");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Tag.EVAL_PAGE;
+    }
+
+    @Override
+    public void release() {
+    
+    }
     
 }
---
+++ src/main/webapp/mytags/usetag4.jsp
@@ -0,0 +1,21 @@
+<%@page import="java.util.Date"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="my" uri="/tlds/mytld" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>
+            <my:bmi h="170.0" w="60.0" />
+        </h1>
+        <h1>
+            <my:bmi h="170.0" w="60.0" result="true" />
+        </h1>
+        <h1>
+            <my:bmi h="170.0" w="60.0" result="false" />
+        </h1>
+    </body>
+</html>
