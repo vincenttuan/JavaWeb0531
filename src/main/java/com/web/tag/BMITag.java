a6ba166c8685cdcf39ea94cfd88875dcb60f9bb4
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-09 19:03:23 +0800

    JavaWeb

---
+++ src/main/java/com/web/tag/BMITag.java
@@ -0,0 +1,13 @@
+package com.web.tag;
+
+/*
+Tag:
+  1. <my:bmi h="170" w="60" />
+  2. <my:bmi h="170" w="60" result="true" />   
+OUTPUT:
+  1. h=170 w=60 bmi=20.76
+  2. h=170 w=60 bmi=20.76 result="正常" (ex:太瘦，太胖...)
+*/
+public class BMITag {
+    
+}
