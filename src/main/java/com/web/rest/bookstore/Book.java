30a095ffa29b5d45a641bfba93676327869cf95b
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-16 20:09:29 +0800

    JavaWeb

--- src/main/java/com/web/rest/bookstore/Book.java
+++ src/main/java/com/web/rest/bookstore/Book.java
@@ -1,5 +1,7 @@
 package com.web.rest.bookstore;
 
+import java.util.Objects;
+
 public class Book {
     private Integer id;
     private String name;
@@ -29,6 +31,41 @@ public class Book {
         this.price = price;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.price);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.price)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     @Override
     public String toString() {
         return "Book{" + "id=" + id + ", name=" + name + ", price=" + price + '}';
--- src/main/java/com/web/rest/bookstore/BookDao.java
+++ src/main/java/com/web/rest/bookstore/BookDao.java
@@ -4,5 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookDao {
+    
+    // 書籍資料庫
     public static List<Book> books = new ArrayList<>();
+    
+    // 多筆查詢
+    public static List<Book> getBooks() {
+        return books;
+    }
+    
+    // 單筆查詢
+    public static Book getBook(Integer id) {
+        return books.stream()
+                .filter(b -> b.getId() == id)
+                .findFirst()
+                .get();
+    } 
+    
+    // 新增
+    public static boolean createBook(Book book) {
+        books.add(book);
+        return true;
+    }
+    
+    // 修改
+    public static boolean updateBook(Integer id, Book book) {
+        // 是否庫存中有此筆資料 ?
+        Book oBook = getBook(id);
+        if(oBook == null) {
+            return false;
+        }
+        // 將 book 的資料給 oBook
+        oBook.setName(book.getName());
+        oBook.setPrice(book.getPrice());
+        return true;
+    }
+    
+    // 修改
+    public static boolean deleteBook(Integer id) {
+        // 是否庫存中有此筆資料 ?
+        Book oBook = getBook(id);
+        if(oBook == null) {
+            return false;
+        }
+        books.remove(oBook);
+        return true;
+    }
 }
