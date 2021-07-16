d5f7cadb61d2b61dad51703dc31dcc1d2a025888
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-16 19:28:40 +0800

    JavaWeb

--- src/main/java/com/web/rest/HelloService.java
+++ src/main/java/com/web/rest/HelloService.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 @Path("/hello")
 public class HelloService {
@@ -17,11 +18,12 @@ public class HelloService {
     }
 
     // Client: /JavaWeb0531/rest/hello/1
-    // Client: /JavaWeb0531/rest/hello/2
+    // Client: /JavaWeb0531/rest/hello/2?score=100
     @Path("/{id}")
     @GET
     @Produces("text/plain")
-    public String helloId(@PathParam("id") Integer id) {
-        return "Hello id = " + id;
+    public String helloId(@PathParam("id") Integer id, 
+                          @QueryParam("score") Integer score) {
+        return "Hello id = " + id + " score = " + score;
     }
 }
