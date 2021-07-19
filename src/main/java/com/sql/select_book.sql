3dc2bb48d0d75e033813c36365ab6e0e004b3b4a
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-19 21:45:27 +0800

    JavaWeb

--- src/main/java/com/sql/select_book.sql
+++ src/main/java/com/sql/select_book.sql
@@ -1,3 +1,11 @@
 select * from Book;
 select name from Book;
 select b.ID, b.TS, b.PRICE, b."NAME" from Book b;
+
+select b."NAME", b.PRICE, b.AMOUNT, (b.PRICE * b.AMOUNT) as subtotal
+from book b
+order by b.PRICE desc; -- asc(預設:小->大), desc(大->小)
+
+select b."NAME", sum(b.PRICE * b.AMOUNT) as subtotal
+from book b
+group by b."NAME";
