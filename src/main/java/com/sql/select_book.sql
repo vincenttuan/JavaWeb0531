37cfeb5845a99bf1f00b61d1ca1b6519f54f1ad2
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-23 19:03:26 +0800

    JavaWeb

--- src/main/java/com/sql/select_book.sql
+++ src/main/java/com/sql/select_book.sql
@@ -9,3 +9,18 @@ order by b.PRICE desc; -- asc(預設:小->大), desc(大->小)
 select b."NAME", sum(b.PRICE * b.AMOUNT) as subtotal
 from book b
 group by b."NAME";
+
+-- 計算每本平均價格
+SELECT b."NAME", SUM(b.PRICE * b.AMOUNT) as subtotal,
+       SUM(b.AMOUNT) as amount, 
+       SUM(b.PRICE * b.AMOUNT) / SUM(b.AMOUNT) as avg_price
+FROM BOOK b
+GROUP BY b."NAME"
+
+-- 全部書籍總價值 ?
+select SUM(b.PRICE * b.AMOUNT) as total from book b
+
+-- Homework 計算每種書籍價值, 佔比
+SELECT b."NAME", SUM(b.PRICE * b.AMOUNT) as subtotal, 0.5 as percent
+FROM BOOK b
+GROUP BY b."NAME";
