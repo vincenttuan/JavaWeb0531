a0570720a8e1cac8b1d4b9c227dcd87147c7da0d
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-26 20:33:29 +0800

    JavaWeb

---
+++ src/main/java/com/web/jpa/entity/Person.java
@@ -0,0 +1,78 @@
+package com.web.jpa.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Person")
+public class Person implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+    
+    @Column(name = "age", nullable = true)
+    private Integer age;
+    
+    @Column
+    private Date ts = new Date();
+
+    public Person() {
+    }
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", age=" + age + ", ts=" + ts + '}';
+    }
+    
+    
+    
+}
